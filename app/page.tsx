a73c5cb13b5e4ec999e83ec7ffb245b90610c87a
dima_samborskyi <samborskiydmitriy1@gmail.com>
2023-09-23 17:38:12 +0300

    add filters for doctors and specialties

--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,34 @@
 import styles from '@/styles/page.module.scss'
 import FormikForm from '@/components/FormikForm'
 
-async function getCities() {
-  const response = await fetch('https://run.mocky.io/v3/9fcb58ca-d3dd-424b-873b-dd3c76f000f4')
+async function getData(category: string) {
+  let res
+  switch (category) {
 
-  return response.json()
+    case 'cities':
+      res = await fetch('https://run.mocky.io/v3/9fcb58ca-d3dd-424b-873b-dd3c76f000f4')
+      break;
+
+    case 'doctors':
+      res = await fetch('https://run.mocky.io/v3/3d1c993c-cd8e-44c3-b1cb-585222859c21')
+      break
+
+    case 'specialties':
+      res = await fetch('https://run.mocky.io/v3/e8897b19-46a0-4124-8454-0938225ee9ca')
+      break
+  }
+  return res?.json()
 }
-
-async function getDoctors() {
-  const response = await fetch('https://run.mocky.io/v3/3d1c993c-cd8e-44c3-b1cb-585222859c21')
-
-  return response.json()
-}
-
-async function getSpecialties() {
-  const response = await fetch('https://run.mocky.io/v3/e8897b19-46a0-4124-8454-0938225ee9ca')
-
-  return response.json()
-}
-
-interface FormValues {
-
-}
-
 
 export default async function Home() {
 
-  const cities = await getCities()
-  const doctors = await getDoctors()
-  const specialties = await getSpecialties()
+  const cities = await getData('cities')
+  const doctors = await getData('doctors')
+  const specialties = await getData('specialties')
 
   return (
     <main className={styles.main}>
-      <FormikForm  cities={cities} doctors={doctors} specialties={specialties}/>
+      <FormikForm cities={cities} doctors={doctors} specialties={specialties} />
     </main>
   )
 }
--- components/FormikForm.tsx
+++ components/FormikForm.tsx
@@ -1,81 +1,182 @@
 'use client'
 
 import styles from '@/styles/page.module.scss'
+
 import type { City, Doctor, Speciality, FormProps } from '@/types/Types'
 import { useFormik, FormikProps } from 'formik'
+import { useMemo } from 'react'
 
 interface FormValues {
-
+    name: string,
+    email: string,
+    date: string,
+    sex: string,
+    cityId: string,
+    specialityId: string,
+    doctorId: string,
 }
 
-const FormikForm = (props: FormProps) => {
+const FormikForm = ({ cities, doctors, specialties }: FormProps) => {
 
-    const formik: FormikProps<FormValues> = useFormik<FormValues>({
-        initialValues: {},
-        onSubmit: () => {
-
+    const { values, handleChange, handleSubmit }: FormikProps<FormValues> = useFormik<FormValues>({
+        initialValues: {
+            name: '',
+            email: '',
+            date: '',
+            sex: '',
+            cityId: '',
+            specialityId: '',
+            doctorId: '',
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
         }
     })
 
+    const filterDoctors = () => {
+        let response = doctors
+        if(values.date){
+            let birthday = new Date(values.date)
+            let age = new Date(Date.now() - birthday.getTime()).getUTCFullYear() - 1970
+            response = response.filter(doctor => age < 18 === doctor.isPediatrician)
+        }
+        if(values.cityId){
+            response = response.filter(doctor => doctor.cityId === values.cityId)
+        }
+        if(values.specialityId){
+            response = response.filter(doctor => doctor.specialityId === values.specialityId)
+        }
+        return response
+    }
+
+    const filterSpecialties = () => {
+        let response = specialties
+        if(values.sex){
+            response = response.filter(speciality => speciality.params?.gender === values.sex || !speciality.params?.gender)
+        }
+        return response
+    }
+
+    const filteredDoctors = useMemo(() => {
+        return filterDoctors()
+    }, [values.date, values.cityId, values.specialityId])
+
+
+    const filteredSpecialties = useMemo(() => {
+        return filterSpecialties()
+    }, [values.sex])
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
 
             <div className={styles.field}>
                 <label htmlFor="name" className={styles.label}>Name</label>
-                <input type="text" id='name' name='name' className={styles.input} />
+                <input
+                    type="text"
+                    id='name'
+                    name='name'
+                    className={styles.input}
+                    value={values.name}
+                    onChange={handleChange}
+                    required
+                />
             </div>
 
             <div className={styles.field}>
                 <label htmlFor="email" className={styles.label}>Email</label>
-                <input type="email" id='email' name='email' className={styles.input} />
+                <input
+                    type="email"
+                    id='email'
+                    name='email'
+                    className={styles.input}
+                    value={values.email}
+                    onChange={handleChange}
+                    required
+                />
             </div>
 
             <div className={styles.field}>
                 <label htmlFor="date" className={styles.label}>Birthday Date</label>
-                <input type="date" id='date' name='date' className={styles.input} />
+                <input
+                    type="date"
+                    id='date'
+                    name='date'
+                    className={styles.input}
+                    value={values.date}
+                    onChange={handleChange}
+                    required
+                />
             </div>
 
             <div className={styles.field}>
                 <label htmlFor="sex" className={styles.label}>Sex</label>
-                <select id='sex' name='sex' className={styles.input}>
+                <select
+                    id='sex'
+                    name='sex'
+                    className={styles.input}
+                    value={values.sex}
+                    onChange={handleChange}
+                    required
+                >
+                    <option value="" hidden>Choose sex</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                 </select>
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="city" className={styles.label}>City</label>
-                <select id='city' name='city' className={styles.input}>
-
-                    {props.cities.map((city: City) => (
-                        <option value="">{city.name}</option>
+                <label htmlFor="cityId" className={styles.label}>City</label>
+                <select
+                    id='cityId'
+                    name='cityId'
+                    className={styles.input}
+                    value={values.cityId}
+                    onChange={handleChange}
+                    required
+                >
+                    <option value="" hidden>Choose city</option>
+                    {cities.map((city: City) => (
+                        <option key={city.id} value={city.id}>{city.name}</option>
                     ))}
 
                 </select>
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="speciality" className={styles.label}>Doctor Speciality</label>
-                <select id='speciality' name='speciality' className={styles.input}>
-
-                    {props.specialties.map((speciality: Speciality) => (
-                        <option value="">{speciality.name}</option>
+                <label htmlFor="specialityId" className={styles.label}>Doctor Speciality</label>
+                <select
+                    id='specialityId'
+                    name='specialityId'
+                    className={styles.input}
+                    value={values.specialityId}
+                    onChange={handleChange}
+                >
+                    <option value="" hidden>Choose speciality</option>
+                    {filteredSpecialties.map((speciality: Speciality) => (
+                        <option key={speciality.id} value={speciality.id}>{speciality.name}</option>
                     ))}
-
                 </select>
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="doctor" className={styles.label}>Doctor</label>
-                <select id='doctor' name='doctor' className={styles.input}>
-
-                    {props.doctors.map((doctor: Doctor) => (
-                        <option value="">{doctor.name} {doctor.surname}</option>
+                <label htmlFor="doctorId" className={styles.label}>Doctor</label>
+                <select
+                    id='doctorId'
+                    name='doctorId'
+                    className={styles.input}
+                    value={values.doctorId}
+                    onChange={handleChange}
+                    required
+                >
+                    <option value="" hidden>Choose doctor</option>
+                    {filteredDoctors.map((doctor: Doctor) => (
+                        <option key={doctor.id} value={doctor.id}>{doctor.name} {doctor.surname}</option>
                     ))}
-                    
+
                 </select>
             </div>
 
+            <button className={styles.button} type='submit'>Submit</button>
         </form>
     )
 }
